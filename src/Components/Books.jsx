9d42b60078082fec5f51bbba360b9fd2ec74fb66
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-04-05 23:41:50 +0600

    add boks

--- src/Components/Books.jsx
+++ src/Components/Books.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useLinkClickHandler, useLoaderData } from "react-router-dom";
 
 const Books = () => {
+  const booksData = useLoaderData();
+  console.log(booksData);
+
   return (
     <div>
       <h1>This is book page</h1>
