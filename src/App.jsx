90845cda05eaff8b7a60905f51595fe80eef05c3
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-04-06 03:43:51 +0600

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Outlet></Outlet>
-      <h1>footer section</h1>
+      <Footer></Footer>
     </div>
   );
 };
